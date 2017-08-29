156cab993a15b4ecfcfd22855e717f22810e87af
kelvincheung <kelvincheung@kiwihk.net>
2017-08-29 12:22:06 +0800

    update cookie

--- backend/controllers/PostController.php
+++ backend/controllers/PostController.php
@@ -39,7 +39,7 @@ class PostController extends Controller
                         'allow' => true,
                     ],
                     [
-                        'actions' => ['logout', 'index','get-post','update','site-redirect','update-post-list'],
+                        'actions' => ['logout', 'index','get-post','update','site-redirect','update-post-list','custom-post'],
                         'allow' => true,
                         'roles' => ['@'],
                     ],
@@ -103,9 +103,9 @@ class PostController extends Controller
                 $session['language'] = 'en-US';
                   $response = $client->createRequest()
                         ->setCookies([
-                        ['name' => '__RequestVerificationToken', 'value' => 'z8vJW3C5szMzvj7-yY_wQzk0n2UQ6aJvSbNyJTJTaO8hSYA9V9mTvM0AVkUMRmwRqhcUdt4kYT28MFVT-d0yK9hXLCwfWHPN0rnszVVbyeU1'],
-                        ['name' => 'COCO01Identity', 'value' => 'ckfc_STyXUhkyqnh8K_BvDkRboQe883Ktg2RW1pEHfrmk6ELz0wsdn2e2s__q1QG2fRHabZ7400xpr8L4h7_m4VL_ePm28hn6ci0NL3AdkKnFH2Q8mrbwTfiiGlq7ANVGkVKdsbByTIV4BZY1ayVbviebsC7utWA58XW2QoCTsdgVVcZ3M9Bt0mQW1suDvCFzDqTlCFK0TEFwPFYH2L9gDEcOqd31-NU94w1bGaiEGSZj_WTML7RNXc00KziwHveZBbvuxcl6pUlBzmXUTeTQ269GqDvJ8IX6u_F0EFiXfBwOqtK0hwlDYfBWejRuAeBV3sfcL0uLMrKeQD2OyqOuQv2WI-1MBr9p81WRuEzcEnZrdFNkiILTn5QhmvL1uPsfAt2Vc_oaSClXVouEVQUK20VLH2j2kJW0IhSqxzGHDF_3x8sSDMAHu9yBocX8Xshm4q-t7cel5YqAeFb3H4nzgIUIFz5-L07weqOJPKObkOqK_K2-kW8ZwCGw2bKmTmV'],
-                        ['name' => '__cfduid', 'value' => 'd7d1d28f36e7af197e327155a0e24c47a1503887581'],
+                        ['name' => '__RequestVerificationToken', 'value' => 'uYRPuGfUFedmSDT7WORgdUXxgk6ACNJ7shaNqkQOG3wwnhoKDypu6PB6Ko8XsldlT6aCAExQBqOny_2OSYgYWHcOs60jk_ZB4sYmz1GagJw1'],
+                        ['name' => 'COCO01Identity', 'value' => 'SOR2R5G1hlYvJPCGFlbBHy4Pu8PXLUhl9YkUDuOa2MA0zyTdahkKg2kMag2OF0Mu95S5qY4Vll4yX96i8PmhhRPW-7hRXTz8jTkmL0cLN0X4c6uZXSG3uIlewU1OnX0dof93HI1qeWSfUQMGpubofEltUtZFfS_NV72vU6Ii_C5STpJhTgdoXw0wSiKDOnKmcYCT4SzYcsIJhzLDOLeAQJpwag9JIWfTmxVC5s0eVGfLN7CdW5wfAXlJeUxTPdhuu0BwsA-UExt4lIpZ4rlo_uxlwPWD8V1BhDRm9DuJY4B9pZNlhP1nLbailqniDBbUWycaeDICyNfCC-v1tmyglC_bzvlGMjBT3c0sdHwU8-eZ9aJl7AV2IiBhcX2tj8JLZbtbAhuOmGg-DQatzOXsuN1bIP78ax12TzMtbGwo0HUNAPU4qz8U003ig-AsQQF9tO0I_AA4_5QOFGtJGnRTK8O9CmNn4uej8UjB2pazBMI4R6Pv3yYclV4kOlq_-v7Y'],
+                        ['name' => '__cfduid', 'value' => 'd6c096ab4f7719a83d8c4a335b39ed19c1503979096'],
                         ['name' => '_ga', 'value' => 'GA1.2.27616028.1503887785'],
                         ['name' => '_gid', 'value' => 'GA1.2.892276425.1503887785'],
                         ])
@@ -243,6 +243,122 @@ class PostController extends Controller
             echo 'error';
         }
 
+    }
+    public function actionUiCustomPost(){
+        $db_category = Category::find()->select(['category_name'])->all();
+
+        $cate_arr = ArrayHelper::map($db_category, 'category_name','category_name');
+            return $this->render('custom_post', [
+                'dropdown_category'=>$cate_arr,
+            ]);
+    }
+    public function actionCustomPost(){
+        $max_page = 2;
+        $max_day = 2;
+        $start_page = 0;
+        $start_day = 1;
+        $page = 2;
+        $date = date('Y-m-d');
+        $onedayTimestamp = 60*60*24;
+        $timestamp_to_day = Yii::$app->formatter->asTimestamp($date);
+        $timestamp_to_pre_day = $timestamp_to_day;
+        $url = 'http://www.coco01.net/user/dailyPost';
+        while($start_day<$max_day){
+            
+            if($start_day!=0){
+                $timestamp_to_pre_day -= $onedayTimestamp;
+                //echo $timestamp_to_pre_day.'<br>';
+                $date = date('Y-m-d',$timestamp_to_pre_day);
+                //echo $date;
+            }
+            while($start_page<$max_page){
+                if($start_page!=0){
+                    $link_params = '?'.'page='.$page.'&date='.$date.'&catId=0';
+                    $request_url = $url.$link_params;
+                }else{
+                    $request_url = $url;
+                }
+                $client = new Client();
+                $session = Yii::$app->session;
+                $session['language'] = 'en-US';
+                  $response = $client->createRequest()
+                        ->setCookies([
+                        ['name' => '__RequestVerificationToken', 'value' => 'uYRPuGfUFedmSDT7WORgdUXxgk6ACNJ7shaNqkQOG3wwnhoKDypu6PB6Ko8XsldlT6aCAExQBqOny_2OSYgYWHcOs60jk_ZB4sYmz1GagJw1'],
+                        ['name' => 'COCO01Identity', 'value' => 'SOR2R5G1hlYvJPCGFlbBHy4Pu8PXLUhl9YkUDuOa2MA0zyTdahkKg2kMag2OF0Mu95S5qY4Vll4yX96i8PmhhRPW-7hRXTz8jTkmL0cLN0X4c6uZXSG3uIlewU1OnX0dof93HI1qeWSfUQMGpubofEltUtZFfS_NV72vU6Ii_C5STpJhTgdoXw0wSiKDOnKmcYCT4SzYcsIJhzLDOLeAQJpwag9JIWfTmxVC5s0eVGfLN7CdW5wfAXlJeUxTPdhuu0BwsA-UExt4lIpZ4rlo_uxlwPWD8V1BhDRm9DuJY4B9pZNlhP1nLbailqniDBbUWycaeDICyNfCC-v1tmyglC_bzvlGMjBT3c0sdHwU8-eZ9aJl7AV2IiBhcX2tj8JLZbtbAhuOmGg-DQatzOXsuN1bIP78ax12TzMtbGwo0HUNAPU4qz8U003ig-AsQQF9tO0I_AA4_5QOFGtJGnRTK8O9CmNn4uej8UjB2pazBMI4R6Pv3yYclV4kOlq_-v7Y'],
+                        ['name' => '__cfduid', 'value' => 'd6c096ab4f7719a83d8c4a335b39ed19c1503979096'],
+                        ['name' => '_ga', 'value' => 'GA1.2.85912979.1503979098'],
+                        ['name' => '_gid', 'value' => 'GA1.2.1326677421.1503979098'],
+                        ['name' => '_gid', 'value' => 'GA1.2.1326677421.1503979098'],
+                        ])
+                      ->setMethod('post')
+                      ->setUrl($request_url)
+                      ->setHeaders(array(
+                        'postman-token' => 'e9b5ea37-d14c-e4ab-c854-dd99c62bc272',
+                        'cache-control' => 'no-cache',
+                        'Content-Length' => 0,
+                      ))->send();
+        
+                  try {
+                      $html = \serhatozles\simplehtmldom\SimpleHTMLDom::str_get_html($response->content);
+                      foreach($html->find('.post-list .list-item') as $element){
+                          //$post_div = \serhatozles\simplehtmldom\SimpleHTMLDom::str_get_html($element);
+                            
+                            foreach($element->find('.title') as $post_title){
+
+                                $disable_pjax_attr ="data-pjax='0'";
+                                $find_post_title = $post_title->outertext;
+                                $substr = 'target="_blank"';
+                                $unable_pjax_title = str_replace($substr,$substr.' '.$disable_pjax_attr,$find_post_title);
+                                foreach($post_title->find('strong') as $tag_strong_title){
+                                    $exists = Post::find()->where(['txt_title'=>$tag_strong_title])->exists(); 
+                                    if($exists){            
+                                        $post = Post::find()->where( ['txt_title'=>$tag_strong_title->plaintext,'title'=>$unable_pjax_title])->one();               
+                                    }else{
+                                        $post = new CreatePost();
+                                        $post->title = $unable_pjax_title;
+                                        $post->txt_title = $tag_strong_title->plaintext;
+                                    }
+                                }
+
+                            }
+                            
+                            //if(findBytitle($post->title))
+                            foreach($element->find('a.username') as $post_author){
+                                $author = preg_replace("/<img[^>]+\>/i", "", $post_author->outertext);
+                                $post->author = $author;
+                            }                    
+                            foreach($element->find('.l-date') as $post_date){
+                                $post->post_date = $post_date->plaintext;
+                            }                      
+                            foreach($element->find('.list-img img') as $post_img){
+                                $post->img_path = $post_img->outertext;
+                            }                    
+                            foreach($element->find('.l-views') as $post_view){
+                                preg_match_all('!\d+!', $post_view, $p_view);
+                                $post->today_view = $p_view[0][0];
+                            }                    
+                            foreach($element->find('.l-catId') as $post_cate){
+                                $post->category = $post_cate->outertext;
+                            }
+                            if($exists){
+                                $post->save();
+                            }else{
+                                $post->createpost();
+                            }
+                      }
+                    $start_page++;
+                    $page++;
+                    
+              } catch (HttpException $ex) {
+                echo $ex;
+              }
+            }
+            $start_day++;
+            
+        }
+        if (!Yii::$app->request->isAjax) {
+            //return $this->redirect(['post/get-post']);
+        }
     }
     public function actionSiteRedirect(){
         $request = Yii::$app->request;
