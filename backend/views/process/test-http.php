dab43b485161697aaea9809b812885aadf1452b6
kelvincheung <kelvincheung@kiwihk.net>
2017-08-31 19:02:16 +0800

    update testcase page

--- backend/views/process/test-http.php
+++ backend/views/process/test-http.php
@@ -1,5 +1,4 @@
 <?php 
-use Yii;
 use yii\web\Controller;
 use yii\filters\VerbFilter;
 use yii\filters\AccessControl;
@@ -18,22 +17,22 @@ use yii\httpclient\Response;
 use Eddmash\Clipboard\Clipboard;
 use yii\base\ErrorException;
 
-echo $userIP = Yii::$app->request->userIP;;
+//var_dump(Yii::$app->request);
+echo \Yii::$app->request->userIP;
+//echo \Yii::$app->request->userHostAddress;
                 
 $client = new Client();
 $session = Yii::$app->session;
 $session['language'] = 'en-US';
 $response = $client->createRequest()
-    ->setMethod('post')
-    ->setUrl('https://test.qooza.hk/ypa_ui/index.html')
-    ->setHeaders(array(
-        'postman-token' => 'e9b5ea37-d14c-e4ab-c854-dd99c62bc272',
-        'cache-control' => 'no-cache',
-        'Content-Length' => 0,
-    ))->send();
-          try {
-              $html = \serhatozles\simplehtmldom\SimpleHTMLDom::str_get_html($response->content);
-          }catch($HttpException e){
-              echo e;
-          }
+    ->setMethod('get')
+    ->setHeaders([''])
+    ->setUrl('http://test.qooza.hk/ypa_ui/index.html')
+    ->send();
+try {
+    $html = \serhatozles\simplehtmldom\SimpleHTMLDom::str_get_html($response->content);
+    echo $html;
+}catch(HttpException $e){
+    echo $e;
+}
 ?>
