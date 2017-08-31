3dc9f9674e47b3a030d3b33aad6d0a8dcffa8004
kelvincheung <kelvincheung@kiwihk.net>
2017-08-31 17:32:21 +0800

    test ypa iframe content

---
+++ backend/views/http/index.php
@@ -0,0 +1,37 @@
+<?php 
+use Yii;
+use yii\web\Controller;
+use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
+use yii\helpers\ArrayHelper;
+use common\models\Post;
+use common\models\Category;
+use backend\models\CreatePost;
+use backend\models\PostSearch;
+
+
+use linslin\yii2\curl;
+use yii\httpclient\Client;
+use yii\httpclient\FormatterInterface;
+use yii\httpclient\ParserInterface;
+use yii\httpclient\Response;
+use Eddmash\Clipboard\Clipboard;
+use yii\base\ErrorException;
+                
+$client = new Client();
+$session = Yii::$app->session;
+$session['language'] = 'en-US';
+$response = $client->createRequest()
+    ->setMethod('post')
+    ->setUrl('https://test.qooza.hk/ypa_ui/index.html')
+    ->setHeaders(array(
+        'postman-token' => 'e9b5ea37-d14c-e4ab-c854-dd99c62bc272',
+        'cache-control' => 'no-cache',
+        'Content-Length' => 0,
+    ))->send();
+          try {
+              $html = \serhatozles\simplehtmldom\SimpleHTMLDom::str_get_html($response->content);
+          }catch($HttpException e){
+              echo e;
+          }
+?>
