84f4398a3ff25722bd2b4a504c28dc6cb7d65489
kelvincheung <kelvincheung@kiwihk.net>
2017-08-31 17:52:12 +0800

    test ypa iframe content

--- backend/controllers/ProcessContoller.php
+++ backend/controllers/ProcessContoller.php
@@ -53,13 +53,8 @@ class ProcessController extends Controller
         ];
     }
 
-    /**
-     * Displays homepage.
-     *
-     * @return string
-     */
-    public function actionIndex(){
-        return $this->render('index');
+    public function actionTestHttp(){
+        return $this->render('test_http');
     }
     public function actionHttpAds()
     {
